136617870168a034545e806bff98c522d79d0b88
bryanvengwa <119483672+bryanvengwa@users.noreply.github.com>
2023-07-10 15:00:38 +0200

    changed link for form submission

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,20 +1,16 @@
-import React from 'react';
-import { FaReact } from 'react-icons/fa';
-import '../default.scss';
+import React from "react";
+import { ClockLoader } from "react-spinners";
+import "../default.scss";
 
 export default function Loader() {
-  return (
+	return (
 		<>
-			<div className="app header">
-				<div className="ball-1"></div>
-				<div className="ball-2"></div>
-				<div className="ball-3"></div>
+			<div className="app">
+				
 				<div className="spinner-container">
-					<FaReact color="white" size={150} />
-					{/* <FaCog color="white" size={150} /> */}
-                                                  
+					<ClockLoader color="green" speed={-10} size={200} />
 				</div>
 			</div>
 		</>
-  );
+	);
 }
