8ac630a3cdebf54f1b2148fc5aa8c93d154c7c81
bryanvengwa <119483672+bryanvengwa@users.noreply.github.com>
2023-07-10 15:17:52 +0200

    modified loader component

--- src/App.js
+++ src/App.js
@@ -10,6 +10,15 @@ const Project = lazy(() => import("./Pages/Project"));
 function App() {
 	const [color, setColor] = useState(1);
 	const [shadow, setShadow] = useState("#00dade79");
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setIsLoading(false);
+		}, 2500);
+
+		return () => clearTimeout(timer);
+	}, []);
 
 	useEffect(() => {
 		const changer = setInterval(() => {
@@ -36,33 +45,43 @@ function App() {
 	// const [themeValue , setThemeValue] = useState('light')
 
 	return (
-		<Suspense fallback={<Loader/>}>
+		<Suspense fallback={<Loader />}>
 			<ThemeContextProvider>
 				<BrowserRouter>
 					<div className="App header">
-							<div className="ball-1"></div>
-							<div className="ball-2"></div>
-							<div className="ball-3"></div>
+						<div className="ball-1"></div>
+						<div className="ball-2"></div>
+						<div className="ball-3"></div>
 						<div className="box">
-							<Routes>
-								<Route
-									path="/"
-									element={
-										<Home color={color} shadow={shadow} />
-									}
-								/>
-								<Route path="/about" element={<About />} />
-								<Route
-									path="/contact"
-									element={
-										<Contacts
-											color={color}
-											shadow={shadow}
-										/>
-									}
-								/>
-								<Route path="/project" element={<Project />} />
-							</Routes>
+							{isLoading ? (
+								<Loader />
+							) : (
+								<Routes>
+									<Route
+										path="/"
+										element={
+											<Home
+												color={color}
+												shadow={shadow}
+											/>
+										}
+									/>
+									<Route path="/about" element={<About />} />
+									<Route
+										path="/contact"
+										element={
+											<Contacts
+												color={color}
+												shadow={shadow}
+											/>
+										}
+									/>
+									<Route
+										path="/project"
+										element={<Project />}
+									/>
+								</Routes>
+							)}
 						</div>
 					</div>
 				</BrowserRouter>
